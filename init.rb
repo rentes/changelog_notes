c78c91ff2bacd9cacba15a79daf1e00b53c4b26e
Miguel Rentes <miguel.rentes@gmail.com>
2015-01-07 15:20:42 +0000

    Creating a project module for the Changelog Notes. Now the project module has to be explicitly enabled on the project settings -> modules.

--- init.rb
+++ init.rb
@@ -5,5 +5,9 @@ Redmine::Plugin.register :changelog_notes do
   version '0.0.1'
   url 'https://github.com/rentes/changelog_notes'
   author_url 'http://about.me/rentes'
-  menu :application_menu, :changelog_notes, { :controller => 'changelog_notes', :action => 'index'}, :caption => 'Changelog Notes'
+  project_module :changelog_notes do
+    permission :view_changelog_notes, :changelog_notes => :index
+    permission :save_changelog_notes, :changelog_notes => :save
+  end
+  menu :project_menu, :changelog_notes, { :controller => 'changelog_notes', :action => 'index'}, :caption => 'Changelog Notes', :after => :activity, :param => :project_id
 end
