71d315b43e6e78f76d38fe5b76379eb2f45b8eda
Miguel Rentes <miguel.rentes@gmail.com>
2015-01-17 20:46:55 +0000

    RuboCop compliant.

--- app/controllers/changelog_notes_controller.rb
+++ app/controllers/changelog_notes_controller.rb
@@ -1,10 +1,11 @@
+# The Changelog Notes Controller
 class ChangelogNotesController < ApplicationController
   unloadable
   include ChangelogNotesControllerHelper
 
   def index
-    get_version_from_combobox
-    get_project_id
+    version_from_combobox
+    project_id
   end
 
   def save
--- app/controllers/changelog_notes_controller_helper.rb
+++ app/controllers/changelog_notes_controller_helper.rb
@@ -1,16 +1,18 @@
+# The Changelog Notes Controller Helper
 module ChangelogNotesControllerHelper
-  def get_project_id
+  def project_id
     project_id_sql = "identifier = \"" + params[:project_id].to_s + "\""
-    @project = Project.all(:conditions => [project_id_sql]).at(0)
+    @project = Project.all(conditions: [project_id_sql]).at(0)
   end
 
-  def get_version_from_combobox
+  def version_from_combobox
     @versions = Project.find(params[:project_id]).shared_versions
-    @sorted_versions = Array.new
+    @sorted_versions = []
     @versions.each do |version|
       @sorted_versions.push(version.name.to_s)
     end
-    @natural_sorted_versions = Naturalsorter::Sorter.sort_version(@sorted_versions, false)
+    @natural_sorted_versions = \
+    Naturalsorter::Sorter.sort_version(@sorted_versions, false)
     @version_selected_combo = params[:id].to_s
   end
 end
