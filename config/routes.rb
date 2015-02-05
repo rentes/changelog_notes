c875f7c0cc048c309d4a78462cbb76043a5a392a
Miguel Rentes <miguel.rentes@gmail.com>
2015-02-05 12:10:03 +0000

    PUT method for saving the issue notes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 # Plugin's routes
 # See: http://guides.rubyonrails.org/routing.html
 get 'changelog_notes', :to => 'changelog_notes#index'
-post 'changelog_notes', :to => 'changelog_notes#save'
+put 'changelog_notes', :to => 'changelog_notes#save'
