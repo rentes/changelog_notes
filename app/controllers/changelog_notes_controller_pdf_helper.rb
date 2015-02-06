ccde3908548b553cfc571ecf0208ca11ecad3eb2
Miguel Rentes <miguel.rentes@gmail.com>
2015-02-06 18:50:03 +0000

    Implementing the saving to PDF functionality.

--- app/controllers/changelog_notes_controller_pdf_helper.rb
+++ app/controllers/changelog_notes_controller_pdf_helper.rb
@@ -3,11 +3,31 @@ module ChangelogNotesControllerPDFHelper
   require 'prawn'
   include ChangelogNotesControllerIssueHelper
 
+  attr_accessor :pdf
+
   # generate a PDF with the selected changelog notes
   def generate_changelog_notes_pdf
-    p "#{@@project} and version: #{@@version_selected_combo}"
-    Prawn::Document.generate('public/hello.pdf') do
-      text 'Hello World!'
+    @pdf = Prawn::Document.new
+    @pdf.text "Changelog Notes for #{@@project} #{@@version_selected_combo}"
+    @pdf.text ''
+    filter_changelog_notes_checked(params[:issue_id],
+                                   params[:issues_changelog_notes_descriptions],
+                                   params[:issues_check_box])
+    @pdf.render_file 'public/changelog_notes.pdf'
+  end
+
+  # from each issue listed on the form, if it is checked then
+  # the note will be added to the PDF
+  def filter_changelog_notes_checked(issues_list, changelog_note, issues_checked)
+    issues_list.each do |issue|
+      if issues_checked[issue] == '1'
+        add_changelog_note_to_pdf(issue, changelog_note[issue])
+      end
     end
+  end
+
+  # processes the request for adding the changelog note to the PDF
+  def add_changelog_note_to_pdf(issue, note)
+    @pdf.text "#{issue} - #{note}"
   end
 end
